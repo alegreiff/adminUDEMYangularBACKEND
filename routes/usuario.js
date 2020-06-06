2793976f9a0dfe1c1d3a179935961b1bb9e16c5d
Jaime de Greiff <alegreiff@gmail.com>
2020-06-06 14:37:24 -0500

    SÁBADO

--- routes/usuario.js
+++ routes/usuario.js
@@ -29,7 +29,8 @@ app.get("/", (req, res, next) => {
       res.status(200).json({
         ok: true,
         usuarios,
-        total: conteo
+        total: conteo,
+        saludos: 'Hola'
       });
     }) 
     
